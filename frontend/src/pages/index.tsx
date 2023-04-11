815d0bc82174bbf0a7d0f668be2ec4910880ac9e
leojunioyuri <leojunioyuri@hotmail.com>
2023-04-10 21:39:54 -0300

    Feat: Navbar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@component/components/header'
+import Navbar from '@component/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,10 @@ const scrollHandler = (e: { preventDefault: () => void }) => {
 
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <Header scrollHandler={scrollHandler} />
+    </>
 /*     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
