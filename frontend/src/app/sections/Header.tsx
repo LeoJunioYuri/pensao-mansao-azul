ea1036c95421b0a559827483d7447f9df36dbbe1
leojunioyuri <leojunioyuri@hotmail.com>
2023-10-09 00:06:00 -0300

    Refactor: otimizando loading background video

--- frontend/src/app/sections/Header.tsx
+++ frontend/src/app/sections/Header.tsx
@@ -1,17 +1,69 @@
 "use client";
+import { useMediaQuery } from "react-responsive";
 import { Button } from "@material-tailwind/react";
-
 //import videoHeader from "../../../public/droneMansaoAzul.mp4";
 // import videoHeader from '../../../public/droneMansaoAzul.mp4';
 
 export default function Header() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
   return (
     <header className="relative shadow-xl overflow-hidden sm:rounded-3xl sm:overflow-visible ">
+      {/* {isDesktopOrLaptop && (
       <div className="absolute inset-x-0 inset-0 blur-[1px]">
         <video autoPlay muted loop className="h-full w-full object-cover">
-        <source src="https://res.cloudinary.com/dff8nl6lq/video/upload/v1696781227/DroneMansaoAzul_jqwemd.mp4" type="video/mp4" />
+          <source src="https://res.cloudinary.com/dff8nl6lq/video/upload/v1696781227/DroneMansaoAzul_jqwemd.mp4" type="video/mp4" />
         </video>
       </div>
+      )}
+      {isTabletOrMobile && (
+      <div className="absolute inset-x-0 inset-0 blur-[1px]">
+      <video autoPlay muted loop className="h-full w-full object-cover">
+        <source src="https://www.leagueoflegends.com/static/hero-de0ba45b1d0959277d12545fbb645722.mp4" type="video/mp4" />
+      </video>
+    </div>
+      )} */}
+      {/* <div className="absolute inset-x-0 inset-0 blur-[1px]">
+        <video autoPlay muted loop className="h-full w-full object-cover">
+          {isDesktopOrLaptop && (
+            <source
+              src="https://res.cloudinary.com/dff8nl6lq/video/upload/v1696781227/DroneMansaoAzul_jqwemd.mp4"
+              type="video/mp4"
+            />
+          )}
+          {isTabletOrMobile && (
+            <source
+              src="https://www.leagueoflegends.com/static/hero-de0ba45b1d0959277d12545fbb645722.mp4"
+              type="video/mp4"
+            />
+          )}
+        </video>
+      </div> */}
+
+      {isTabletOrMobile ? (
+        <div className="absolute inset-x-0 inset-0 blur-[1px]">
+          <video autoPlay muted loop className="h-full w-full object-cover">
+            <source
+              src="https://www.leagueoflegends.com/static/hero-de0ba45b1d0959277d12545fbb645722.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      ) : (
+        <div className="absolute inset-x-0 inset-0 blur-[1px]">
+          <video autoPlay muted loop className="h-full w-full object-cover">
+            <source
+              src="https://res.cloudinary.com/dff8nl6lq/video/upload/f_auto:video,q_auto/DroneMansaoAzul_jqwemd"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      )}
+
+
+
       <div className="relative px-4 py-24 sm:py-32 md:py-32 lg:py-48 sm:max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl mx-auto">
         <p className="text-center text-lg md:text-xl lg:text-2xl font-semibold tracking-wide mb-8 md:mb-12 lg:mb-16 mt-6 text-gray-300">
           O lugar certo para você ficar em Lavras-MG
