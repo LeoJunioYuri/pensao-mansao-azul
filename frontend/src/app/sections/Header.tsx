36b17ead2b6f1ca4e420660aa74fdc7fc24ab70e
leojunioyuri <leojunioyuri@hotmail.com>
2023-10-09 11:40:51 -0300

    fix: ajuste cor texto header modo claro

--- frontend/src/app/sections/Header.tsx
+++ frontend/src/app/sections/Header.tsx
@@ -48,14 +48,14 @@ export default function Header() {
         </div>
 
       <div className="relative px-4 py-24 sm:py-32 md:py-32 lg:py-48 sm:max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl mx-auto">
-        <p className="text-center text-lg md:text-xl lg:text-2xl font-semibold tracking-wide mb-8 md:mb-12 lg:mb-16 mt-6 text-white-300">
+        <p className="text-center text-lg md:text-xl lg:text-2xl font-semibold tracking-wide mb-8 md:mb-12 lg:mb-16 mt-6 text-white">
           O lugar certo para você ficar em Lavras-MG
         </p>
         <h1 className="text-center font-extrabold uppercase text-gray-900 text-3xl sm:text-5xl md:text-6xl lg:text-7xl mb-4 md:mb-6 lg:mb-8">
           <span className="text-white">Pensão Mansão</span>
           <span className="text-blue-700"> Azul</span>
         </h1>
-        <p className="text-center text-lg md:text-xl lg:text-2xl font-semibold tracking-wide mb-8 md:mb-12 lg:mb-16 mt-6 text-white-500">
+        <p className="text-center text-lg md:text-xl lg:text-2xl font-semibold tracking-wide mb-8 md:mb-12 lg:mb-16 mt-6 text-white">
           Mais de 20 anos de tradição e qualidade
         </p>
 
